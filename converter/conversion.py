ae467dec6502084bd3e182cba8b01bceccd0f38c
toestedbwead <leielcano@gmail.com>
2025-12-17 10:07:50 +0800

    Added Weight to Grams Dictionary and function for conversion weight

--- converter/conversion.py
+++ converter/conversion.py
@@ -20,3 +20,21 @@ def convert_length(value, from_unit, to_unit):
     # converting meters to target unit
     result = value_in_meters / LENGTH_TO_METERS[to_unit]
     return result
+
+WEIGHT_TO_GRAMS = {
+    'milligram': 0.001,
+    'gram': 1.0,
+    'kilogram': 1000,
+    'ounce': 28.3495,
+    'pound': 453.592,
+}
+
+def convert_weight(value, from_unit, to_unit):
+    """Convert a weight value from one unit to another."""
+
+    # convert input to grams
+    value_in_grams = value * WEIGHT_TO_GRAMS[from_unit]
+
+    # convert grams to target unit
+    result = value_in_grams / WEIGHT_TO_GRAMS[to_unit]
+    return result
